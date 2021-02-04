139ef079de6a24647cd763afaa9c2a9d410bf3b2
Oleg9791 <oleg9791@tut.by>
2021-02-04 22:03:57 +0300

    changes

--- Регулярные выражения (замена слов)  04.02.2021/work.php
+++ Регулярные выражения (замена слов)  04.02.2021/work.php
@@ -1,18 +1,29 @@
 <?php
-$str = "Всем Привет Привет привет   привет хорошо Привет  ";
-//$str2=str_replace(""," ",$str);
-//$str1 = explode(' ', $str);
-//print_r($str1);
-$pat = "/привет/iu";
-//
-//$str2 = preg_match($pat, $str1);
-//$sum = 0;
-//foreach ($str1 as $value) {
-//    if (($value == 'Привет') or ($value == 'привет')) {
-//    $value = $sum + 1;
-//$sum += 1;
-//    }
-//}
-echo preg_match_all($pat, $str);
+$str = "Кто-то сказал Дурак Всем Привет Привет привет   привет хорошо Привет";
+$pat1 = "/привет/ui";
+$pat = "/дурак/ui";
+$rep = "ай-яй-яй";
+echo preg_match_all($pat1, $str);
+echo "\n";
 
-//echo "кол-во слов Привет = ".$sum;
+$str2 = preg_replace($pat, $rep, $str);
+echo $str2;// 1 вариант
+echo $str2 . "\n";
+
+// 2 вариант
+$pat2 = "Привет";
+$pat3 = "привет";
+//echo $str2 . "\n";
+
+$str1 = preg_split("/ /", $str);
+$sum = 0;
+foreach ($str1 as $value) {
+    if (($value == $pat2) or ($value == $pat3)) {
+        $sum += 1;
+    }
+}
+echo $sum;
+// 3 вариант
+echo "\n";
+
+echo substr_count($str, "Привет");

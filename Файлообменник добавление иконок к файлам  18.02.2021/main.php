7f55d0ffc1bf72c0ee9935218e9a345378d32006
Oleg9791 <oleg9791@tut.by>
2021-02-18 18:20:43 +0300

    ch

---
+++ Файлообменник добавление иконок к файлам  18.02.2021/main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <img src="img\word.png" alt="картинка">
+    <img src="img\pdf.png" alt="картинка">
+    <img src="img\cel.png" alt="картинка">
+    <img src="img\dword.png" alt="картинка">
+</div>
+<br>
+
+<form enctype="multipart/form-data" action="up.php" method="POST">
+    <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="3000000000000000000"/>
+    <!-- Название элемента input определяет имя в массиве $_FILES -->
+    Отправить этот файл: <input name="userfile" type="file"/>
+    <input type="submit" value="Отправить файл"/>
+</form>
+<br>
+<?php
+include "showdir.php";
+?>
+</body>
+</html>
+</html>
